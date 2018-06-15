4523ea3c1950b93fa4825fe1b5b13160d477979b
Narahari Sundaragopalan <nsundaragopalan@unomaha.edu>
2018-06-15 11:46:16 -0500

    String reversal exercise

---
+++ exercises/reverseString.js
@@ -0,0 +1,33 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+// 3 Possible solutions
+
+// Using Array reverse function
+function reverse(str) {
+  return str.split('').reverse().join('');
+}
+
+// Using for loop
+function reverse(str) {
+  let reversed = '';
+
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+
+  return reversed;
+}
+
+// Using reduce function
+// The reduce function takes 2 arguments
+// First argument below is a function, and second argument is an empty string
+
+function reverse(str) {
+  return str.split('').reduce((rev, char) => char + rev, '');
+}
