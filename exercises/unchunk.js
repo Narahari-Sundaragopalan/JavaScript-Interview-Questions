7621d0d9073c9e29a6b9dbb423bdf0f687b53882
Narahari Sundaragopalan <nsundaragopalan@unomaha.edu>
2018-06-22 15:16:19 -0500

    Update unchunk.js

--- exercises/unchunk.js
+++ exercises/unchunk.js
@@ -6,17 +6,17 @@
 
 function unChunkArray() {
   let unchunked = [];
-	const arr = [[1,2], [3,4,5], [6], 7, [8]];
+  const arr = [[1,2], [3,4,5], [6], 7, [8]];
 
-	for(let element of arr) {
-		if(Array.isArray(element)) {
-  		for(let e of element) {
-      	unchunked.push(e);
-  	}
- 	} else {
+  for(let element of arr) {		
+    if(Array.isArray(element)) {
+      for(let e of element) {
+    	unchunked.push(e);
+      }
+    } else {
     	unchunked.push(element);
     }  
-	}
+  }
   return unchunked;
 }
 
