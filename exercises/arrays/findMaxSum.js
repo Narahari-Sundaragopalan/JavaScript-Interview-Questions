02d26b39883cfb439ee01ad7f47c9f3f72eaff2a
Narahari Sundaragopalan <nsundaragopalan@unomaha.edu>
2018-07-17 23:56:05 -0500

    Update findMaxSum.js

--- exercises/arrays/findMaxSum.js
+++ exercises/arrays/findMaxSum.js
@@ -1,12 +1,12 @@
 // Given an array containing both negative and positive integers.
 // Find the contiguous sub-array with maximum sum.
 
-findMaxSum(arr) {
+function findMaxSum(arr) {
   let globalMaxSum = arr[0];
-  let currentMaxSum = arr[0];
+  let currentMaxSum = 0;
 
-  for (let i = 0; i < arr.length; i++) {
-    currentMaxSum = Math.max(currentMaxSum, currentMaxSum + arr[i]);
+  for (let element of arr) {
+    currentMaxSum = currentMaxSum + element;
 
     if (currentMaxSum > globalMaxSum) {
       globalMaxSum = currentMaxSum;
