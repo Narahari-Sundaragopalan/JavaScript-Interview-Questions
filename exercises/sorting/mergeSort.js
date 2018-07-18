b4a02e8c5ddcdc0c6fed99b9653a37d255468454
Narahari Sundaragopalan <nsundaragopalan@unomaha.edu>
2018-07-18 00:09:36 -0500

    Update mergeSort.js

--- exercises/sorting/mergeSort.js
+++ exercises/sorting/mergeSort.js
@@ -8,8 +8,8 @@ function mergeSort(arr) {
   }
 
   const center = Math.floor(arr.length / 2);
-  const left = arr.splice(0, center);
-  const right = arr.splice(center);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
 
   return merge(mergeSort(left), mergeSort(right));
 }
