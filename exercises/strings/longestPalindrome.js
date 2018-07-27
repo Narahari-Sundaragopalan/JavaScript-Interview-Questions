122d902d7e668dfc7170450e93ed1953537f8d25
Narahari Sundaragopalan <nsundaragopalan@unomaha.edu>
2018-07-27 12:53:36 -0500

    Set initial value to Longest Palindrome problem

--- exercises/strings/longestPalindrome.js
+++ exercises/strings/longestPalindrome.js
@@ -11,14 +11,20 @@
 // Output: "bb"
 
 function longestPalindrome(str) {
-  let longest = '';
+  if (str.length === 0 || str.length === 1) {
+        return str;
+    }
+  let longest = str[0];
   for (let i = 0; i < str.length; i++) {
+
     let currentCharacter = str[i];
     let firstMatch = '';
     for (let j = i + 1; j < arr.length; j++) {
+
       if (currentCharacter === str[j]) {
         firstMatch = j;
         const substring = str.slice(i, firstMatch + 1);
+
         if (substring.length > longest.length &&
           substring.toLowerCase() === substring.split('').reverse().join('').toLowerCase()) {
             longest = substring;
