fe2a84d235dd671c8ac7d560b29f0f23c2299027
Narahari Sundaragopalan <nsundaragopalan@unomaha.edu>
2019-11-20 09:52:42 -0600

    Update reverse word

--- exercises/strings/reverseEachWord.js
+++ exercises/strings/reverseEachWord.js
@@ -2,7 +2,9 @@
 // "Welcome to this Javascript Guide!" should be become "emocleW ot siht tpircsavaJ !ediuG"
 
 function reverseBySeparator(string, separator) {
-  return string.split(separator).reverse().join(separator);
+  return string.split(separator)
+                .map(word => word.split('').reverse().join(''))
+                .join(separator);
 }
 
 // To test
@@ -14,3 +16,42 @@ function reverseBySeparator(string, separator) {
 //
 // // Output becomes emocleW ot siht tpircsavaJ
 // let reverseWord = reverseBySeparator(reverseBySentence, " ");
+
+// Without using split and reverse functions
+
+const reverseEachWord = str => {
+	let words = split(str);
+	let result = '';
+
+	for (let word of words) {
+		result = result + reverse(word) + ' ';
+	}
+
+	return result.trim();
+}
+
+const split = str => {
+	const words = [];
+	let word = '';
+
+	for (let i = 0; i < str.length; i++) {
+		if (str.charAt(i) === ' ') {
+			words.push(word);
+			word = '';
+		} else {
+			word += str.charAt(i);
+		}
+	}
+	words.push(word);
+	return words;
+}
+
+const reverse = word => {
+	let result = '';
+
+	for (let i = word.length - 1; i >= 0; i--) {
+		result += word[i];
+	}
+
+	return result;
+}
