5419d4ff76aee18d3802b1d456b786e26f98717f
Narahari Sundaragopalan <nsundaragopalan@unomaha.edu>
2019-12-04 00:18:27 -0600

    updated canJump

--- exercises/leetcode/google/canJump.js
+++ exercises/leetcode/google/canJump.js
@@ -1,3 +1,25 @@
+/**
+ * Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Determine if you are able to reach the last index.
+
+Example 1:
+
+Input: [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum
+             jump length is 0, which makes it impossible to reach the last index.
+ */
+
+ // O(n2) : Time Complexity
+ // O(n) : Space Complexity
 const INDEX = {
     'G': 'GOOD',
     'B': 'BAD',
@@ -22,4 +44,18 @@ const canJump = nums => {
     }
 
     return map[0] == INDEX.G;
+}
+
+// Greedy Approach
+
+const canJump = nums => {
+	let lastPos = nums.length - 1;
+
+	for (let i = nums.length - 1; i >= 0; i--) {
+		if (i + nums[i] >= lastPos) {
+			lastPos = i;
+		}
+	}
+
+	return lastPos == 0;
 }
